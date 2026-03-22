7d9d1c9ae4b704fbd78f298779b11c6b555a9b1a
AdoleNG <victorameh2011@gmail.com>
2026-03-21 21:42:12 -0300

    fix syntax issue in package.json

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "./supabaseClient.jsx";
+import { supabase } from "./supabaseClient";
 import VerseCastLogo from "./assets/VerseCastLogo.png";
 
 export default function Dashboard() {
